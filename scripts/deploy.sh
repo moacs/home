69b81e9eb6fb82805252f0733961355773fc3aec
moacs <moacs@msn.com>
2023-09-17 13:01:17 +0800

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,7 @@
-mv home/.git dist/.git
-rm -r home
-mv dist home
 # find ./home/* -maxdepth 1  | grep -v "\(.git\)" 
-# find ./home/* -maxdepth 1  | grep -v "\(.git\)" | xargs rm -rf "{}"
-# cp -af ./dist/* ./home/
-
-git -C home commit -a -m "deploy"
-git -C home push
+find ./home/* -maxdepth 1  | grep -v "\(.git\)" | xargs rm -rf "{}"
+cp -af ./dist/* ./home/
+cd home
+git add .
+git commit -a -m "deploy"
+git push
